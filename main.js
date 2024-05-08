859b64337b9afe75a69400bcbb405de195fc5e28
Kehindebrowns <111283128+Kehindebrowns@users.noreply.github.com>
2024-05-08 07:04:11 -0600

    updates
    updates

---
+++ main.js
@@ -0,0 +1,9 @@
+import file from "./file";
+console.log(file);
+console.log(import.meta.env);
+
+if(import.meta.env.DEV){
+    console.log("this is a server")
+}
+document.querySelector("h1").className = classtitle;
+console.log("Hello world");
